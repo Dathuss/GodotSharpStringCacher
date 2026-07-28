f7dc6fa11b3f9ff0e2e1a9881ff8539a85445835
Dathuss <34245959+Dathuss@users.noreply.github.com>
2026-07-28 19:01:38 +0200

    Remove warnings generated by MSBuild task (use source analyzers now)

--- GodotSharpStringCacher.MSBuild/Logger.cs
+++ GodotSharpStringCacher.MSBuild/Logger.cs
@@ -11,10 +11,6 @@ namespace GodotSharpStringCacher.MSBuild;
 /// <param name="task">The target MSBuild task</param>
 public class Logger(Task task) : LoggerBase
 {
-	public IReadOnlyCollection<SerializedWarningLog> Warnings => _warnings;
-
-	readonly List<SerializedWarningLog> _warnings = [];
-
 	public override void LogMessage(string message)
 	{
 		task.Log.LogMessage(message);
@@ -27,15 +23,11 @@ public class Logger(Task task) : LoggerBase
 
 	public override void LogWarning(string message)
 	{
-		_warnings.Add(new SerializedWarningLog(message, null, 0, 0, 0, 0));
-
 		task.Log.LogWarning(message);
 	}
 
 	public override void LogWarning(string file, int lineNumber, int columnNumber, int endLineNumber, int endColumnNumber, string message)
 	{
-		_warnings.Add(new SerializedWarningLog(message, file, lineNumber, columnNumber, endLineNumber, endColumnNumber));
-
 		task.Log.LogWarning(null, null, null, file, lineNumber, columnNumber, endLineNumber, endColumnNumber, message);
 	}
 
@@ -48,14 +40,4 @@ public class Logger(Task task) : LoggerBase
 	{
 		task.Log.LogError(null, null, null, file, lineNumber, columnNumber, endLineNumber, endColumnNumber, message);
 	}
-
-	[DataContract]
-	public readonly record struct SerializedWarningLog(
-		[property: DataMember] string Message,
-		[property: DataMember] string? File,
-		[property: DataMember] int Line,
-		[property: DataMember] int Column,
-		[property: DataMember] int EndLine,
-		[property: DataMember] int EndColumn
-	);
 }
